08d0ec024e173ea79eb77e3e55123e55dfa4d2a2
Rana <rana.alghidany@gmail.com>
2026-05-15 02:58:55 +0300

    Complete Lab 12: Registration, Login and Messages

--- bookmodule/views.py
+++ bookmodule/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book, Publisher, Author
 from django.db.models import Sum, F, ExpressionWrapper, FloatField, Min
-from .forms import BookForm  
+from .forms import BookForm 
+from django.contrib.auth.decorators import login_required 
+from django.contrib import messages 
 
+
+@login_required(login_url='/users/login')
 def lab9_task1(request):
     total_quantity = Book.objects.aggregate(total=Sum('quantity'))['total'] or 1
     books = Book.objects.annotate(
@@ -13,20 +17,24 @@ def lab9_task1(request):
     )
     return render(request, 'bookmodule/lab9_task1.html', {'books': books})
 
+@login_required(login_url='/users/login')
 def lab9_task2(request):
     publishers = Publisher.objects.annotate(total_stock=Sum('book__quantity'))
     return render(request, 'bookmodule/lab9_task2.html', {'publishers': publishers})
 
+@login_required(login_url='/users/login')
 def lab9_task3(request):
     publishers = Publisher.objects.annotate(oldest_book_date=Min('book__pubdate'))
     return render(request, 'bookmodule/lab9_task3.html', {'publishers': publishers})
 
 
 
+@login_required(login_url='/users/login')
 def lab10_listbooks(request):
     books = Book.objects.all()
     return render(request, 'bookmodule/lab10_listbooks.html', {'books': books})
 
+@login_required(login_url='/users/login')
 def lab10_addbook(request):
     if request.method == 'POST':
         t = request.POST.get('title')
@@ -37,11 +45,13 @@ def lab10_addbook(request):
         
         publisher_obj = Publisher.objects.get(id=pub_id)
         Book.objects.create(title=t, price=p, quantity=q, pubdate=pd, publisher=publisher_obj)
-        return redirect('/books/lab9_part1/listbooks')
+        messages.success(request, "Book added successfully (Part 1)!") # رسالة نجاح
+        return redirect('/books/lab10/listbooks')
     
     publishers = Publisher.objects.all()
     return render(request, 'bookmodule/lab10_addbook.html', {'publishers': publishers})
 
+@login_required(login_url='/users/login')
 def lab10_editbook(request, id):
     book = get_object_or_404(Book, id=id)
     if request.method == 'POST':
@@ -50,42 +60,52 @@ def lab10_editbook(request, id):
         book.quantity = request.POST.get('quantity')
         book.pubdate = request.POST.get('pubdate')
         book.save()
-        return redirect('/books/lab9_part1/listbooks')
+        messages.info(request, "Book updated successfully!")
+        return redirect('/books/lab10/listbooks')
     return render(request, 'bookmodule/lab10_editbook.html', {'book': book})
 
+@login_required(login_url='/users/login')
 def lab10_deletebook(request, id):
     book = get_object_or_404(Book, id=id)
     book.delete()
-    return redirect('/books/lab9_part1/listbooks')
+    messages.warning(request, "Book deleted!")
+    return redirect('/books/lab10/listbooks')
 
 
 
+@login_required(login_url='/users/login')
 def lab10_listbooks2(request):
     books = Book.objects.all()
     return render(request, 'bookmodule/lab10_listbooks2.html', {'books': books})
 
+@login_required(login_url='/users/login')
 def lab10_addbook2(request):
     if request.method == "POST":
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/books/lab9_part2/listbooks')
+            messages.success(request, "Book added successfully using Django Forms!")
+            return redirect('/books/lab10/listbooks2')
     else:
         form = BookForm()
     return render(request, 'bookmodule/lab10_addbook2.html', {'obj': form})
 
+@login_required(login_url='/users/login')
 def lab10_editbook2(request, id):
     book = get_object_or_404(Book, id=id)
     if request.method == "POST":
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('/books/lab9_part2/listbooks')
+            messages.info(request, "Book updated successfully!")
+            return redirect('/books/lab10/listbooks2')
     else:
         form = BookForm(instance=book)
     return render(request, 'bookmodule/lab10_editbook2.html', {'obj': form})
 
+@login_required(login_url='/users/login')
 def lab10_deletebook2(request, id):
     book = get_object_or_404(Book, id=id)
     book.delete()
-    return redirect('/books/lab9_part2/listbooks')
+    messages.warning(request, "Book deleted successfully!")
+    return redirect('/books/lab10/listbooks2')
--- bookwebsite/settings.py
+++ bookwebsite/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookmodule', # تأكدي أن تطبيقك مضاف هنا
+    'bookmodule', 
+    'users',
 ]
 
 MIDDLEWARE = [
